da481f38c3bade7699d75306af923b68df0bfc75
Maciej Makowski <maciejm.githubatcfiet.net>
2019-10-25 01:08:34 +0100

    Same issue with `run` instead of `run_return`

--- src/main.rs
+++ src/main.rs
@@ -9,11 +9,10 @@ use winit::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop, EventLoopProxy},
     window::WindowBuilder,
-    platform::desktop::EventLoopExtDesktop
 };
 
 fn main() {
-    let mut event_loop = EventLoop::new();
+    let event_loop = EventLoop::new();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
     let event_loop_proxy: EventLoopProxy<()> = event_loop.create_proxy();
     
@@ -35,7 +34,7 @@ fn main() {
         }
     });
 
-    event_loop.run_return(move |event, _, control_flow| {
+    event_loop.run(move |event, _, control_flow| {
         // Artificailly slowing down so spammer always overproduces events
         thread::sleep(Duration::from_millis(1));
 
